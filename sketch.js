25f7d25ef14da5753a1fb244f7b03b4ffd591130
GarF1eldGo <2854919592@qq.com>
2023-12-20 21:23:52 -0500

    [Update Sec] : add sec circle

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
+let cnt = 0;
+let angle = 0.0;
+let prevSec = -1;
+
 // setup() is called once at page-load
 function setup() {
-    createCanvas(800,600); // make an HTML canvas element width x height pixels
+    let canvas = createCanvas(windowWidth, windowHeight); // make an HTML canvas element width x height pixels
+    canvas.style('display', 'block')
+}
+
+// resize canvas when the browser window resizes
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+    background(123)
 }
 
 // draw() is called 60 times per second
@@ -17,4 +28,41 @@ function draw() {
     text(min, 10, 60);
     fill(0);
     text(sec, 10, 90);
+
+    drawMeteor(sec);
+
+    stroke(0);
+    line(0, height / 2, width, height / 2);
+    line(width / 2, 0, width / 2, height);
+
+    
+}
+
+function drawMeteor(sec) {
+    // calculate angle
+    if (sec != prevSec) {
+        angle = map(sec, 0, 60, 0, TWO_PI) - HALF_PI - 3 * PI / 180; // consider meteor's size
+        cnt = 0;
+        prevSec = sec;
+    }
+
+    if (cnt != 0) {
+        angle += 0.1 * Math.PI / 180;
+    }
+
+    width = 300;
+    height = 300;
+    x = width / 2 + 100 * cos(angle);
+    y = height / 2 + 100 * sin(angle);
+
+    // calculate gradient color
+    gradientColor = lerpColor(color(0, 191, 114), color(5, 25, 55), sec/60);
+
+    // draw meteor
+    noStroke();
+    fill(gradientColor);
+    ellipse(x, y, 20, 20);
+
+    // update cnt
+    cnt += 1;
 }
